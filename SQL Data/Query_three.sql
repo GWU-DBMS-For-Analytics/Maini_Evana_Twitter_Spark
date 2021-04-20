72bd9b1efe1516921a4b2f8846ac2145249fe3cd
Madhavi Evana <44774264+mevana@users.noreply.github.com>
2021-04-19 20:24:49 -0400

    Delete Query_three.sql

--- SQL Data/Query_three.sql
+++
@@ -1,6 +0,0 @@
-SELECT u.user_id, u.screen_name, count(s.user_id) as NumofTweets,sum(s.retweet_count) as NumofRetweets,
- sum(s.retweet_count)/count(s.user_id) as Total FROM statuses s
-inner join users u
-on u.user_id=s.user_id
-group by 1 
-order by total desc limit 5;
